9e33d658db484cb90a1429dfaf4a35875670b490
RPC <rpc0027@alu.ubu.es>
2018-12-26 20:49:06 +0100

    Commented wait.c, wait.h and wait_config.h.

--- tfm/source/wait.c
+++ tfm/source/wait.c
@@ -1,33 +1,57 @@
-/** @file wait.c
+/**
+ * @file wait.c
+ * @author RPC0027
+ * @date December 2018
+ * @version 1.0
  *
- * @brief Creates waiting cycles on the CPU.
+ * @brief Create waiting cycles on the CPU.
  *
  */
+
+/*******************************************************************************
+ * Includes
+ ******************************************************************************/
 #include "wait.h"
 
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+/**
+ * @brief Wait 10 CPU cycles.
+ *
+ * This function will wait 10 CPU cycles (including call overhead).
+ * lint -save -e522 function lacks side effect.
+ * NOTE: Cortex-M0 and M4 have 1 cycle for a NOP.
+ * Compiler is GNUC.
+ */
 #ifdef __GNUC__
-  #ifdef __cplusplus  /* gcc 4.7.3 in C++ mode does not like no_instrument_function: error: can't set 'no_instrument_function' attribute after definition */
+#ifdef __cplusplus  /* gcc 4.7.3 in C++ mode does not like no_instrument_function: error: can't set 'no_instrument_function' attribute after definition */
   __attribute__((naked))
-  #else
+#else
   __attribute__((naked, no_instrument_function))
-  #endif
+#endif
 #endif
 void wait_wait0Cycles(void)
 {
-  /* This function will wait 10 CPU cycles (including call overhead). */
-  /*lint -save -e522 function lacks side effect. */
-  /* NOTE: Cortex-M0 and M4 have 1 cycle for a NOP */
-  /* Compiler is GNUC */
-  __asm (
-   /* bl wait0Cycles() to here: [4] */
-   "nop   \n\t" /* [1] */
-   "nop   \n\t" /* [1] */
-   "nop   \n\t" /* [1] */
-   "bx lr \n\t" /* [3] */
-  );
-  /*lint -restore */
+	/*lint -save -e522 function lacks side effect. */
+	__asm (
+			/* bl to here   [4] */
+			"nop   \n\t" /* [1] */
+			"nop   \n\t" /* [1] */
+			"nop   \n\t" /* [1] */
+			"bx lr \n\t" /* [3] */
+	);
+	/*lint -restore */
 }
 
+/**
+ * @brief Wait 100 CPU cycles.
+ *
+ * This function will wait 10 CPU cycles (including call overhead).
+ * lint -save -e522 function lacks side effect.
+ * NOTE: Cortex-M0 and M4 have 1 cycle for a NOP.
+ * Compiler is GNUC.
+ */
 #ifdef __GNUC__
 #ifdef __cplusplus  /* gcc 4.7.3 in C++ mode does not like no_instrument_function: error: can't set 'no_instrument_function' attribute after definition */
   __attribute__((naked))
@@ -37,105 +61,133 @@ void wait_wait0Cycles(void)
 #endif
 void wait_wait00Cycles(void)
 {
-  /* This function will spend 100 CPU cycles (including call overhead). */
-  /*lint -save -e522 function lacks side effect. */
-  __asm (
-   /* bl to here:               [4] */
-   "push {r0}   \n\t"        /* [2] */
-   "movs r0, #0 \n\t"        /* [1] */
-   "loop:       \n\t"
-   "nop         \n\t"        /* [1] */
-   "nop         \n\t"        /* [1] */
-   "nop         \n\t"        /* [1] */
-   "nop         \n\t"        /* [1] */
-   "add r0,#1   \n\t"        /* [1] */
-   "cmp r0,#9   \n\t"        /* [1] */
-   "bls loop    \n\t"        /* [3] taken, [1] not taken */
-   "nop         \n\t"        /* [1] */
-   "nop         \n\t"        /* [1] */
-   "nop         \n\t"        /* [1] */
-   "nop         \n\t"        /* [1] */
-   "nop         \n\t"        /* [1] */
-   "nop         \n\t"        /* [1] */
-   "nop         \n\t"        /* [1] */
-   "pop {r0}    \n\t"        /* [2] */
-   "bx lr       \n\t"        /* [3] */
-  );
-  /*lint -restore */
+	/*lint -save -e522 function lacks side effect. */
+	__asm (
+			/* bl to here:               [4] */
+			"push {r0}   \n\t"        /* [2] */
+			"movs r0, #0 \n\t"        /* [1] */
+			"loop:       \n\t"
+			"nop         \n\t"        /* [1] */
+			"nop         \n\t"        /* [1] */
+			"nop         \n\t"        /* [1] */
+			"nop         \n\t"        /* [1] */
+			"add r0,#1   \n\t"        /* [1] */
+			"cmp r0,#9   \n\t"        /* [1] */
+			"bls loop    \n\t"        /* [3] taken, [1] not taken */
+			"nop         \n\t"        /* [1] */
+			"nop         \n\t"        /* [1] */
+			"nop         \n\t"        /* [1] */
+			"nop         \n\t"        /* [1] */
+			"nop         \n\t"        /* [1] */
+			"nop         \n\t"        /* [1] */
+			"nop         \n\t"        /* [1] */
+			"pop {r0}    \n\t"        /* [2] */
+			"bx lr       \n\t"        /* [3] */
+	);
+	/*lint -restore */
 }
 
+/**
+ * @brief Wait the specified CPU cycles.
+ *
+ * @param[in] cycles the number of CPU cycles to wait.
+ */
 void wait_WaitCycles(uint16_t cycles)
 {
-  /*lint -save -e522 function lacks side effect. */
+	/*lint -save -e522 function lacks side effect. */
 #if wait_CONFIG_USE_CYCLE_COUNTER
-  uint32_t counter = cycles;
+	uint32_t counter = cycles;
+	counter += KIN1_GetCycleCounter();
 
-  counter += KIN1_GetCycleCounter();
-  while(KIN1_GetCycleCounter()<counter) {
-    /* wait */
-  }
+	while (KIN1_GetCycleCounter() < counter)
+	{
+		/* wait */
+	}
 #else
-  while(cycles > 100) {
-    wait_wait00Cycles();
-    cycles -= 100;
-  }
-  while(cycles > 10) {
-    wait_wait0Cycles();
-    cycles -= 10;
-  }
+	while (cycles > 100)
+	{
+		wait_wait00Cycles();
+		cycles -= 100;
+	}
+
+	while (cycles > 10)
+	{
+		wait_wait0Cycles();
+		cycles -= 10;
+	}
 #endif
-  /*lint -restore */
+	/*lint -restore */
 }
 
+
+/**
+ * @brief Wait the specified CPU cycles.
+ *
+ * @param[in] cycles the long number of CPU cycles to wait.
+ */
 void wait_WaitLongCycles(uint32_t cycles)
 {
 #if wait_CONFIG_USE_CYCLE_COUNTER
-  uint32_t counter = cycles;
+	uint32_t counter = cycles;
+	counter += KIN1_GetCycleCounter();
 
-  counter += KIN1_GetCycleCounter();
-  while(KIN1_GetCycleCounter()<counter) {
-    /* wait */
-  }
+	while (KIN1_GetCycleCounter() < counter)
+	{
+		/* wait */
+	}
 #else
-  /*lint -save -e522 function lacks side effect. */
-  while(cycles>60000) {
-    wait_WaitCycles(60000);
-    cycles -= 60000;
-  }
-  wait_WaitCycles((uint16_t)cycles);
-  /*lint -restore */
+	/*lint -save -e522 function lacks side effect. */
+	while (cycles > 60000)
+	{
+		wait_WaitCycles(60000);
+		cycles -= 60000;
+	}
+
+	wait_WaitCycles((uint16_t) cycles);
+	/*lint -restore */
 #endif
 }
 
+/**
+ * @brief Wait the specified milliseconds.
+ *
+ * @param[in] ms the milliseconds to wait.
+ */
 void wait_Waitms(uint16_t ms)
 {
-  /*lint -save -e522 function lacks side effect. */
-  uint32_t msCycles; /* cycles for 1 ms */
+	/*lint -save -e522 function lacks side effect. */
+	uint32_t msCycles;
+	msCycles = WAIT_NofCyclesMs(1, WAIT_INSTR_CLOCK_HZ);
 
-  /* static clock/speed configuration */
-  msCycles = WAIT_NofCyclesMs(1, WAIT_INSTR_CLOCK_HZ);
-  while(ms>0) {
-    wait_WaitLongCycles(msCycles);
-    ms--;
-  }
-  /*lint -restore */
+	while (ms > 0)
+	{
+		wait_WaitLongCycles(msCycles);
+		ms--;
+	}
+	/*lint -restore */
 }
 
+/**
+ * @brief Init the cycle counter.
+ */
 void wait_Init(void)
 {
 #if wait_CONFIG_USE_CYCLE_COUNTER
-  /* init cycle counter */
-  KIN1_InitCycleCounter();
-  KIN1_ResetCycleCounter();
-  KIN1_EnableCycleCounter();
+	/* init cycle counter */
+	KIN1_InitCycleCounter();
+	KIN1_ResetCycleCounter();
+	KIN1_EnableCycleCounter();
 #endif
 }
 
+/**
+ * @brief Deinit the cycle counter.
+ */
 void wait_DeInit(void)
 {
 #if wait_CONFIG_USE_CYCLE_COUNTER
-  /* disable hardware cycle counter */
-  KIN1_DisableCycleCounter();
+	/* disable hardware cycle counter */
+	KIN1_DisableCycleCounter();
 #endif
 }
 
--- tfm/source/wait_config.h
+++ tfm/source/wait_config.h
@@ -1,30 +1,35 @@
-/** @file wait_config.h
+/**
+ * @file wait.h
+ * @author RPC0027
+ * @date December 2018
+ * @version 1.0
  *
- * @brief Configuration header file for wait.
+ * @brief Configuration for the waiting cycles on the CPU.
  *
- * @par
- * This header file is used to configure settings of the busy waiting module.
  */
 
 #ifndef WAIT_CONFIG_H
 #define WAIT_CONFIG_H
 
-// If 1: use hardware cycle counter (if present, only on Cortex-M3 or higher).
-// If 0: not using hardware cycle counter.
-//
+/**
+ * If 1: use hardware cycle counter (if present, only on Cortex-M3 or higher).
+ * If 0: not using hardware cycle counter.
+ */
 #ifndef WAIT_CONFIG_USE_CYCLE_COUNTER
 #define WAIT_CONFIG_USE_CYCLE_COUNTER  (0 && (__CORTEX_M >= 3))
 #endif
 
-// If 1: use RTOS wait if RTOS is present.
-// If 0: use normal busy waiting.
-//
+/**
+ * If 1: use RTOS wait if RTOS is present.
+ * If 0: use normal busy waiting.
+ */
 #ifndef WAIT_CONFIG_USE_RTOS_WAIT
 #define WAIT_CONFIG_USE_RTOS_WAIT  (0 && USE_RTOS)
 #endif
 
-// Include Cortex utility functions.
-//
+/**
+ * Include Cortex utility functions.
+ */
 #if WAIT_CONFIG_USE_CYCLE_COUNTER
 #include "KIN1.h"
 #endif
