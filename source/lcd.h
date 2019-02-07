c3caac32ecf976550a8fe098192739c1ffbfdf57
RPC <rpc0027@alu.ubu.es>
2019-02-07 21:13:44 +0100

    Add license notice.

--- source/lcd.h
+++ source/lcd.h
@@ -1,4 +1,20 @@
 /**
+ * Copyright 2019 rpc0027
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
  * @file lcd.h
  * @author RPC0027
  * @date December 2018
@@ -8,7 +24,9 @@
  *
  * Provides several functions to carry out operations with the display.
  *
- * @see https://github.com/fdebrabander/Arduino-LiquidCrystal-I2C-library/blob/master/LiquidCrystal_I2C.h
+ * Created by: Frank de Brabander
+ * @see https://github.com/fdebrabander/Arduino-LiquidCrystal-I2C-library
+ * Modified by: rpc0027
  */
 
 #ifndef LCD_H
